ff50a641283048ec49e57e3e328cce21cc7f8de0
Amer-bit <ny.1995@outlook.com>
2020-04-05 20:20:24 +0300

    locationFeatureAlmostComplete

--- server.js
+++ server.js
@@ -11,31 +11,58 @@ const server = express();
 server.use(cors());
 
 //API Routes
-server.get('/location', (request , response)=>{
-try {
-    const geoData = require('./data/geo.json');
-    const city = request.query.city
-    const cityData = new CityInfo(city, geoData)
-    console.log(cityData);
-    response.status(200).json(cityData);
-} catch (error) {
-    errorHandler(error, request, response)
-}
-    
-    
+server.get('/location', (request, response) => {
+    try {
+        const geoData = require('./data/geo.json');
+        const city = request.query.city
+        const cityData = new CityInfo(city, geoData)
+        response.status(200).json(cityData);
+    } catch (error) {
+        errorHandler(error, request, response)
+    }
 });
 
-function CityInfo(city,geoData){
-    this.search_query= city;
+server.get('/weather', (request, response) => {
+    try {
+        const weatherData = require('./data/darksky.json');
+
+        weatherData.data.forEach(value => {
+            let weatherDesc = value.weather.description;
+            let date = value.datetime
+            new WeatherInfo(weatherDesc, date)
+        });
+        response.status(200).json(cityWeatherArrayOfObj);
+
+    } catch (error) {
+        errorHandler(error, request, response)
+    }
+
+});
+
+
+
+
+function CityInfo(city, geoData) {
+    this.search_query = city;
     this.formatted_query = geoData[0].display_name;
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
 };
 
-function errorHandler(){
+let cityWeatherArrayOfObj = [];
+console.log('hey', cityWeatherArrayOfObj);
+function WeatherInfo(weatherData, date) {
+    this.forecast = weatherData;
+    this.time = date;
+    cityWeatherArrayOfObj.push(this);
+};
+
+
+function errorHandler() {
     response.status(500).send(error);
 }
 
 // Make sure the server is listening to the upcoming request
-server.listen(PORT, () => {console.log(`The server is up and running on ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`The server is up and running on ${PORT}`);
 })
